3e2d0fcf9d6c60c8cfa4e80c93934d48c5c10d73
imizao <lai.jinwen@vpclub.cn>
2023-02-22 10:21:49 +0800

    完善功能,并删除最后一个元素

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use hashbrown::HashMap;
 
 fn main() {
     assert_eq!(number_to_zhcn(123456), "一十二万三千四百五十六".to_string());
-    assert_eq!(number_to_zhcn(211133456), "二亿一千一百一十三万三千四百五十六".to_string());
-    assert_eq!(number_to_zhcn(10013000), "一千零一万三千零".to_string());
-    assert_eq!(number_to_zhcn(1000123000), "一十亿一十二万三千零".to_string());
-    assert_eq!(number_to_zhcn(1007890000), "一十亿七百八十九万".to_string());
+    assert_eq!(number_to_zhcn(211133456),"二亿一千一百一十三万三千四百五十六".to_string());
+    assert_eq!(number_to_zhcn(10013000), "一千零一万三千".to_string());
+    assert_eq!(number_to_zhcn(1000123000),"一十亿零一十二万三千".to_string());
+    assert_eq!(number_to_zhcn(1007890000),"一十亿零七百八十九万".to_string());
     assert_eq!(number_to_zhcn(100000000000), "一千亿".to_string());
-    assert_eq!(number_to_zhcn(100000123100), "数字不可以大于一千亿！".to_string());
+    assert_eq!(number_to_zhcn(100000123100),"数字不可以大于一千亿！".to_string());
 }
 
 fn number_to_zhcn(number: i64) -> String {
@@ -24,7 +24,7 @@ fn number_to_zhcn(number: i64) -> String {
     map.insert("9", "九");
 
     let unit = vec![
-        " ", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千"
+        " ", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千",
     ];
     let num: i64 = 100000000000;
     if number > num {
@@ -43,7 +43,7 @@ fn number_to_zhcn(number: i64) -> String {
                 } else {
                     unit[index].to_string()
                 };
-                name = format!("{}{un}{name}",value.to_string());
+                name = format!("{}{un}{name}", value.to_string());
                 index += 1;
             }
         }
@@ -52,9 +52,19 @@ fn number_to_zhcn(number: i64) -> String {
     name = name.replace("零零", "零");
     name = name.replace("零万", "万");
     name = name.replace("零亿", "亿");
-    name = name.replace("万零", "万");
-    name = name.replace("亿零", "亿");
     name = name.replace("亿万", "亿");
-    println!("{name}");
-    return name;
+
+    let name_arr: Vec<&str> = name.split("").collect();
+    let mut middle = &name_arr[1..name_arr.len() - 1];
+    let last_name = "零".to_string();
+
+    if let Some(&last_elem) = middle.last() {
+        if *last_elem.to_string() == last_name {
+            // 如果最后一个元素是 零，删除它
+            middle = &name_arr[1..name_arr.len() - 2];
+        }
+    }
+    let new_name: String = middle.into_iter().map(|c| c.to_string()).collect();
+    println!("{new_name}");
+    return new_name;
 }
