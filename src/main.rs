f3ec67b2048e8ec932b5029d7c3957e5ba152ae9
imizao <lai.jinwen@vpclub.cn>
2023-02-22 11:47:51 +0800

    优化,最后元素为 零 的情况

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     assert_eq!(number_to_zhcn(123456), "一十二万三千四百五十六".to_string());
     assert_eq!(number_to_zhcn(211133456), "二亿一千一百一十三万三千四百五十六".to_string());
     assert_eq!(number_to_zhcn(10013000), "一千零一万三千".to_string());
+    assert_eq!(number_to_zhcn(102013000), "一亿零二百零一万三千".to_string());
     assert_eq!(number_to_zhcn(1000123000), "一十亿零一十二万三千".to_string());
     assert_eq!(number_to_zhcn(1007890000), "一十亿零七百八十九万".to_string());
     assert_eq!(number_to_zhcn(100000000000), "一千亿".to_string());
@@ -43,7 +44,11 @@ fn number_to_zhcn(number: i64) -> String {
                 } else {
                     unit[index].to_string()
                 };
-                name = format!("{}{un}{name}", value.to_string());
+                name = if name.is_empty() && value == &"零" && index < 4 {
+                    String::from("")
+                } else {
+                    format!("{}{un}{name}", value.to_string())
+                };
                 index += 1;
             }
         }
@@ -54,17 +59,17 @@ fn number_to_zhcn(number: i64) -> String {
     name = name.replace("零亿", "亿");
     name = name.replace("亿万", "亿");
 
-    let name_arr: Vec<&str> = name.split("").collect();
-    let mut middle = &name_arr[1..name_arr.len() - 1];
-    let last_name = "零".to_string();
+    // let name_arr: Vec<&str> = name.split("").collect();
+    // let mut middle = &name_arr[1..name_arr.len() - 1];
+    // let last_name = "零".to_string();
 
-    if let Some(&last_elem) = middle.last() {
-        if *last_elem.to_string() == last_name {
-            // 如果最后一个元素是 零，删除它
-            middle = &name_arr[1..name_arr.len() - 2];
-        }
-    }
-    let new_name: String = middle.into_iter().map(|c| c.to_string()).collect();
-    println!("{new_name}");
-    return new_name;
+    // if let Some(&last_elem) = middle.last() {
+    //     if *last_elem.to_string() == last_name {
+    //         // 如果最后一个元素是 零，删除它
+    //         middle = &name_arr[1..name_arr.len() - 2];
+    //     }
+    // }
+    // let new_name: String = middle.into_iter().map(|c| c.to_string()).collect();
+    println!("{name}");
+    return name;
 }
