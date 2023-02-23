497641a6d41f2efd13c604b8a98a457aed360810
imizao <lai.jinwen@vpclub.cn>
2023-02-23 15:37:43 +0800

    完善显示优化

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use hashbrown::HashMap;
 use regex::Regex;
 
-
 fn main() {
-    assert_eq!(number_to_zhcn(123456), "一十二万三千四百五十六");
-    assert_eq!(number_to_zhcn(211133456), "二亿一千一百一十三万三千四百五十六");
+    assert_eq!(number_to_zhcn(123456), "十二万三千四百五十六");
+    assert_eq!(number_to_zhcn(211133456), "两亿一千一百一十三万三千四百五十六");
     assert_eq!(number_to_zhcn(10013000), "一千零一万三千");
     assert_eq!(number_to_zhcn(102013000), "一亿零二百零一万三千");
-    assert_eq!(number_to_zhcn(1000123000), "一十亿零一十二万三千");
-    assert_eq!(number_to_zhcn(1007890000), "一十亿零七百八十九万");
+    assert_eq!(number_to_zhcn(1000123000), "十亿零一十二万三千");
+    assert_eq!(number_to_zhcn(1007890000), "十亿零七百八十九万");
     assert_eq!(number_to_zhcn(1000000000001), "数字不可以大于一千亿！");
     assert_eq!(number_to_zhcn(100000000000), "一千亿");
 }
@@ -43,10 +42,8 @@ fn number_to_zhcn(number: i64) -> String {
     let mut index = 0;
     let re = Regex::new(r"零{2,}").unwrap();
 
-    for i in str_arr.iter() {
-        if i.is_empty() {
-            continue;
-        }
+    for i in str_arr.iter().filter(|i| !i.is_empty()) {
+        
         let value = map.get(i).unwrap_or(&"");
         let un = match value {
             &"零" if index % 4 != 0 || index < 4 => "",
@@ -54,9 +51,10 @@ fn number_to_zhcn(number: i64) -> String {
         };
         let new_str = match value {
             &"零" if  index < 4 => String::new(),
+            &"一" if  index == str.len() -1 && un == "十" => format!("{}", un),
+            &"二" if  index == str.len() -1 => format!("{}{}", "两", un),
             _ => format!("{}{}", value, un),
         };
-   
         name = format!("{}{}", new_str, name);
         index += 1;
     }
