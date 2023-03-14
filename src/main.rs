aebd6ed5aff28a9a586dbd23a40259d6f36aae6c
imizao <2313466338@qq.com>
2023-03-14 23:49:35 +0800

    reduce replace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use hashbrown::HashMap;
 use lazy_static::lazy_static;
-use std::io;
+use std::{env, io};
 
 lazy_static! {
     static ref MAP: HashMap<&'static str, &'static str> = {
@@ -17,13 +17,15 @@ lazy_static! {
         map.insert("9", "九");
         map
     };
-    static ref UNIT: Vec<&'static str> = vec!["", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千"];
+    static ref UNIT: Vec<&'static str> =
+        vec!["", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千"];
     static ref TOO_LARGE: &'static str = "数字不可以大于一千亿！";
     static ref ZERO: &'static str = "零";
     static ref MAX_NUMBER: i64 = 100000000000;
 }
 
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     loop {
         let mut input = String::new();
         println!("Please input your number: \n");
@@ -70,8 +72,7 @@ impl Conversion {
 
         let num_to_str = number.to_string();
         let mut index = 1;
-        let mut cn_to_read = String::new();
-        let mut last_char_is_zero = 0;
+        let mut cn_to_vec: Vec<String> = vec![];
         for i in num_to_str.chars() {
             let value = MAP.get(i.to_string().as_str()).unwrap_or(&"");
             let current_index = num_to_str.len() - index;
@@ -79,10 +80,8 @@ impl Conversion {
             let new_str = match *value {
                 "零" => {
                     if current_index % 4 == 0 {
-                        last_char_is_zero = 0;
                         format!("{}", un)
                     } else if current_index < 4 {
-                        last_char_is_zero += 1;
                         match num_to_str[index..num_to_str.len()].parse::<i64>() {
                             Ok(n) if n == 0 => String::new(),
                             Err(_) => String::new(),
@@ -91,35 +90,32 @@ impl Conversion {
                             }
                         }
                     } else {
-                        last_char_is_zero += 1;
                         format!("{}", value)
                     }
                 }
                 "一" if index == 1 && un == "十" => {
-                    last_char_is_zero = 0;
                     format!("{}", un)
                 }
                 "二" if index == 1 && un != "十" && num_to_str.len() > 1 => {
-                    last_char_is_zero = 0;
                     format!("{}{}", "两", un)
                 }
                 _ => {
-                    last_char_is_zero = 0;
                     format!("{}{}", value, un)
                 }
             };
-
-            if last_char_is_zero <= 1 {
-                cn_to_read = format!("{}{}", cn_to_read, new_str);
+            if new_str == "零" && cn_to_vec.ends_with(&[ZERO.to_string()]) {
+            } else if new_str == "万" || new_str == "亿" && cn_to_vec.ends_with(&[ZERO.to_string()])
+            {
+                cn_to_vec.pop();
+                cn_to_vec.push(new_str);
+            } else {
+                cn_to_vec.push(new_str);
             }
+
             index += 1;
         }
-
-        cn_to_read = cn_to_read
-            .replace("零万", "万")
-            .replace("零亿", "亿")
-            .replace("亿万", "亿");
-
+        let mut cn_to_read = cn_to_vec.join("");
+        cn_to_read = cn_to_read.replace("亿万", "亿");
         cn_to_read
     }
 }
